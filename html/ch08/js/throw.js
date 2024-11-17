31a504dbba59d4f3fb68495d76a165497a9dbff0
G2N <gr2nade@gmail.com>
2024-11-17 23:53:13 +0900

    throw

---
+++ html/ch08/js/throw.js
@@ -0,0 +1,44 @@
+$(document).ready(() => {
+    let guessAnswer;
+
+    function initialize() {
+        eventHandler();
+    }
+
+    function eventHandler() {
+        let $buttonArr = $(document).find('button');
+
+        $buttonArr.on('click', (event) => {
+            let $targetParent = $(event.target).parent();
+
+            if ($targetParent.hasClass('number_guess')) {
+                callNumberGuess($targetParent, event);
+            }
+        })
+    }
+
+    function callNumberGuess($targetParent, event) {
+        if (!guessAnswer) {
+            guessAnswer = (Math.floor(Math.random() * 100)) + 1;
+        }
+        let $numberInput = $targetParent.find('input.number_input');
+        let $result = $targetParent.find('input.result');
+        console.log($numberInput.val());
+        console.log(guessAnswer);
+        if ($numberInput.val()) {
+            try {
+                let userInput = $numberInput.val();
+
+                if (userInput > guessAnswer) throw "Down!!";
+                if (userInput < guessAnswer) throw "Up!!";
+                if (userInput == guessAnswer) throw "정답!!";
+            } catch (error) {
+                $result.val('Error: ' + error);
+            }
+        } else {
+            $result.val('Error : Empty Input Number');
+        }
+    }
+
+    initialize();
+});
